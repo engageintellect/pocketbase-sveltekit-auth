74e678dda9d77b5d9f33f95ac27f26a2eae1ffec
engageintellect <jc9361@gmail.com>
2024-04-19 20:25:34 -0700

    init chatGPT

--- src/routes/ai/chat/+page.svelte
+++ src/routes/ai/chat/+page.svelte
@@ -2,12 +2,19 @@
   import { useChat } from 'ai/svelte'
   const { input, handleSubmit, messages } = useChat()
   import Icon from '@iconify/svelte'
+  import { fade, slide } from 'svelte/transition'
 </script>
 
 <section class="">
   {#if $messages.length < 1}
-    <div class="flex flex-col w-full">
-      <div class="flex gap-2 items-center">
+    <div
+      transition:slide={{ delay: 0, duration: 500 }}
+      class="flex flex-col w-full"
+    >
+      <div
+        transition:fade={{ delay: 0, duration: 200 }}
+        class="flex gap-2 items-center"
+      >
         <Icon icon="simple-icons:openai" class="w-10 h-10" />
         <h1 class="text-3xl">chat gpt</h1>
       </div>
@@ -18,7 +25,10 @@
   <div class="sticky top-0 bg-base-100 w-full">
     <form class="bg-base-100 py-2 top-0 w-full" on:submit={handleSubmit}>
       <div class="flex gap-2">
+        <!-- TODO: This can be cleaner -->
+        <!-- svelte-ignore a11y-autofocus -->
         <input
+          autofocus
           bind:value={$input}
           class="w-full input input-bordered focus:outline-none"
         />
@@ -36,11 +46,11 @@
       {#each $messages as message}
         {#if message.role === 'user'}
           <div>
-            <span class="text-primary">{message.role}</span>: {message.content}
+            <span class="text-primary">{message.role}</span>: {@html message.content}
           </div>
         {:else}
           <div>
-            <span class="text-primary">{message.role}</span>: {message.content}
+            <span class="text-primary">{message.role}</span>: {@html message.content}
           </div>
         {/if}
       {/each}
